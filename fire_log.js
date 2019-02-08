886a1564b7f2f8b6bda7819a14fb075187e792bb
Am-Coder <mishraaman2210@gmail.com>
2019-02-08 13:56:20 +0530

    Doc to firebase done

--- fire_log.js
+++ fire_log.js
@@ -11,14 +11,16 @@
 
   var emailField = document.getElementById("email");
   var pswField = document.getElementById("password");
-  const btLogin = document.getElementById("btLogin");
+  const btLogin = document.getElementById("btlogin");
 
   btLogin.addEventListener("click",e => {
     const email=emailField.value;
     const psw=pswField.value;
     const auth=firebase.auth();
     const promise=auth.signInWithEmailAndPassword(email,psw);
-    promise.catch(e => console.log(e.message));
+    promise.catch(e => alert(e.message));
+    window.location.href.replace("doctor_login","doctor_home");
+
 })
 
 
