cafef021023ecb68251609a892b0b48b95429c26
Am-Coder <mishraaman2210@gmail.com>
2019-02-08 13:34:54 +0530

    Firebase connection of doctor

---
+++ fire_log.js
@@ -0,0 +1,31 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDvvnPLQMzF3JOdg-ddsdV5sah6FpEpOsY",
+    authDomain: "doctalkfinal-ce5b5.firebaseapp.com",
+    databaseURL: "https://doctalkfinal-ce5b5.firebaseio.com",
+    projectId: "doctalkfinal-ce5b5",
+    storageBucket: "doctalkfinal-ce5b5.appspot.com",
+    messagingSenderId: "1039956260614"
+  };
+  firebase.initializeApp(config);
+
+  var emailField = document.getElementById("email");
+  var pswField = document.getElementById("password");
+  const btLogin = document.getElementById("btLogin");
+
+  btLogin.addEventListener("click",e => {
+    const email=emailField.value;
+    const psw=pswField.value;
+    const auth=firebase.auth();
+    const promise=auth.signInWithEmailAndPassword(email,psw);
+    promise.catch(e => console.log(e.message));
+})
+
+
+firebase.auth().onAuthStateChanged(firebaseUser =>  {
+    if(firebaseUser){
+        console.log(firebaseUser);
+    }else{
+        console.log("not logged in");
+    }
+})
---
+++ fire_sign.js
@@ -0,0 +1,50 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDvvnPLQMzF3JOdg-ddsdV5sah6FpEpOsY",
+    authDomain: "doctalkfinal-ce5b5.firebaseapp.com",
+    databaseURL: "https://doctalkfinal-ce5b5.firebaseio.com",
+    projectId: "doctalkfinal-ce5b5",
+    storageBucket: "doctalkfinal-ce5b5.appspot.com",
+    messagingSenderId: "1039956260614"
+  };
+  firebase.initializeApp(config);
+
+  var emailField = document.getElementById("email");
+  var pswField = document.getElementById("password");
+  var nameField = document.getElementById("f-name");
+  var regnoField = document.getElementById("med-reg-no");
+  var stateField = document.getElementById("stateslist");
+  var phField = document.getElementById("phone-no");
+  var btSignup = document.getElementById("btSign");
+
+  btSignup.addEventListener("click",e => {
+    const email=emailField.value;
+    const psw=pswField.value;
+    const name = nameField.value;
+    const regno = regnoField.value;
+    const state = stateField.options[stateField.selectedIndex].value;
+    const phNo = phField.value;
+    const auth=firebase.auth();
+    const promise=auth.createUserWithEmailAndPassword(email,psw);
+    promise.catch(e => console.log(e.message));
+})
+// btSignup.addEventListener("click",e => {
+//     const email=emailField.value;
+//     const psw=pswField.value;
+//     console.log("asd-"+email+psw);
+//     const auth=firebase.auth();
+//     const promise=auth.createUserWithEmailAndPassword(email,psw);
+//     promise
+//             .catch(e => console.log(e.message));
+
+//     // console.log(promise);
+
+// })
+
+firebase.auth().onAuthStateChanged(firebaseUser =>  {
+    if(firebaseUser){
+        console.log(firebaseUser);
+    }else{
+        console.log("not logged in");
+    }
+})
